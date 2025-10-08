3e89408916d4cf4d7e88630fa45b72f77c619500
ajaajahmad <ajaaj75@gmail.com>
2025-10-08 23:06:48 +0530

    Rest Assured

---
+++ src/test/java/com/automation/tests/ValidateEmployee.java
@@ -0,0 +1,18 @@
+package com.automation.tests;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+
+public class ValidateEmployee {
+
+	@Test
+	public void verifyUser() {
+		given().when().get("http://dummy.restapiexample.com/api/v1/employee/2").then().statusCode(200)
+				.statusLine("HTTP/1.1 200 OK").body("data.empployee_name", equalTo("Garrett Winters"))
+				.body("message", equalTo("Successfully! Record has been fetched."));
+
+	}
+
+}
