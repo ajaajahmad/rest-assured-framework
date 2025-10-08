55750b2ccade3a809bcefb866f9b6c2bb82e9e08
ajaajahmad <ajaaj75@gmail.com>
2025-10-08 23:07:54 +0530

    Rest Assured

--- src/test/java/com/automation/tests/ValidateEmployee.java
+++ src/test/java/com/automation/tests/ValidateEmployee.java
@@ -9,8 +9,12 @@ public class ValidateEmployee {
 
 	@Test
 	public void verifyUser() {
-		given().when().get("http://dummy.restapiexample.com/api/v1/employee/2").then().statusCode(200)
-				.statusLine("HTTP/1.1 200 OK").body("data.empployee_name", equalTo("Garrett Winters"))
+		given().when()
+				.get("http://dummy.restapiexample.com/api/v1/employee/2")
+				.then()
+				.statusCode(200)
+				.statusLine("HTTP/1.1 200 OK")
+				.body("data.empployee_name", equalTo("Garrett Winters"))
 				.body("message", equalTo("Successfully! Record has been fetched."));
 
 	}
