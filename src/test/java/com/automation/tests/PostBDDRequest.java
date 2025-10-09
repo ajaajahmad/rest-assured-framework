18d035728936a50275a8bd2b0825a905e48ec2b0
ajaajahmad <ajaaj75@gmail.com>
2025-10-09 12:36:56 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PostBDDRequest.java
+++ src/test/java/com/automation/tests/PostBDDRequest.java
@@ -1,31 +1,39 @@
 package com.automation.tests;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.anyOf;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
-
 import io.restassured.http.ContentType;
-import static io.restassured.RestAssured.given;
 import io.restassured.response.ValidatableResponse;
 
 public class PostBDDRequest {
 
-	ValidatableResponse validate;
+    ValidatableResponse validate;
 
-	@Test
-	public void createUser() {
+    @Test
+    public void createUser() {
 
-		String jsonString = "{\"name\":\"ajaaj\",\"salary\":\"2000\",\"age\":\"26\"}";
-		
-		validate = given()
-					.baseUri("https://dummy.restapiexample.com/api")
-					.contentType(ContentType.JSON)
-					.body(jsonString)
-					.when().post()
-					.then()
-					.assertThat().statusCode(200).body("data.name", equalTo("ajaaj"))
-					.body("message", equalTo("Successfully! Record has been added."));
-		
-		System.out.println("Response : " + validate.extract().asPrettyString());
-	}
+        Map<String, String> jsonBody = new HashMap<>();
+        jsonBody.put("name", "ajaaj");
+        jsonBody.put("salary", "2000");
+        jsonBody.put("age", "26");
+
+        validate = given()
+                    .baseUri("https://dummy.restapiexample.com/api")
+                    .contentType(ContentType.JSON)
+                    .body(jsonBody)
+                    .when().post("/v1/create")
+                    .then()
+                    .assertThat()
+                    .statusCode(anyOf(equalTo(200), equalTo(201)))
+                    .body("data.name", equalTo("ajaaj"))
+                    .body("message", equalTo("Successfully! Record has been added."));
+
+        System.out.println("Response : " + validate.extract().asPrettyString());
+    }
 }
