2b1fe8c959523daff10933785bb22362bd11f67a
ajaajahmad <ajaaj75@gmail.com>
2025-10-09 22:06:34 +0530

    Rest Assured

---
+++ src/test/java/com/automation/tests/PutNonBDDRequest.java
@@ -0,0 +1,22 @@
+package com.automation.tests;
+
+import org.junit.Test;
+
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class PutNonBDDRequest {
+	
+	RequestSpecification request;
+	ValidatableResponse validate;
+	Response response;
+	
+	
+	@Test
+	public void updateUser() {
+		
+		
+	}
+
+}
