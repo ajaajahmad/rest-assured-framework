474dd04e1f1a25110bc3f88879777cb5e5612bac
ajaajahmad <ajaaj75@gmail.com>
2025-10-10 11:57:36 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PutNonBDDRequest.java
+++ src/test/java/com/automation/tests/PutNonBDDRequest.java
@@ -1,5 +1,8 @@
 package com.automation.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import io.restassured.response.Response;
@@ -7,16 +10,22 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 public class PutNonBDDRequest {
-	
+
 	RequestSpecification request;
 	ValidatableResponse validate;
 	Response response;
-	
-	
+
 	@Test
 	public void updateUser() {
-		
-		
+
+		Map<String, String> jsonString = new HashMap<>();
+
+		jsonString.put("id", "2");
+		jsonString.put("employee_name", "Garrett Winters");
+		jsonString.put("employee_salary", "90908");
+		jsonString.put("employee_age", "46");
+		jsonString.put("profile_image", "");
+
 	}
 
 }
