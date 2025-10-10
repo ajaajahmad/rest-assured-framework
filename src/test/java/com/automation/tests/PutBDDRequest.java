020eb1822c46033b98b41036d530b4dc39ee9a40
ajaajahmad <ajaaj75@gmail.com>
2025-10-10 12:56:13 +0530

    Rest Assured

---
+++ src/test/java/com/automation/tests/PutBDDRequest.java
@@ -0,0 +1,23 @@
+package com.automation.tests;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import io.restassured.response.ValidatableResponse;
+
+public class PutBDDRequest {
+
+	ValidatableResponse validate;
+	
+	@Test
+	public void updateUser() {
+		
+		Map<String, String> jsonString = new HashMap<>();
+		
+		
+		
+	}
+
+}
