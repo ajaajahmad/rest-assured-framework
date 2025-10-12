848f100cbd0e033512cc6d67bc6816dbaee41050
ajaajahmad <ajaaj75@gmail.com>
2025-10-12 23:43:10 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PatchNonBDDRequest.java
+++ src/test/java/com/automation/tests/PatchNonBDDRequest.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
@@ -21,7 +22,7 @@ public class PatchNonBDDRequest {
 		Map<String, String> map = new HashMap<>();
 		map.put("name", "William");
 		
-		
+		RestAssured.baseURI = "https://reqres.in/api/users/2";
 	}
 
 }
