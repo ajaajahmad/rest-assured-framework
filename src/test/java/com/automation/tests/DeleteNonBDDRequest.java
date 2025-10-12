f42e3254775c536f2f0480ba6501348d934d4277
ajaajahmad <ajaaj75@gmail.com>
2025-10-12 22:49:13 +0530

    Rest Assured

--- src/test/java/com/automation/tests/DeleteNonBDDRequest.java
+++ src/test/java/com/automation/tests/DeleteNonBDDRequest.java
@@ -1,10 +1,23 @@
 package com.automation.tests;
 
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
 public class DeleteNonBDDRequest {
-	
+
+	RequestSpecification request;
+	Response response;
+	ValidatableResponse validate;
+
+	@Test
 	public void deleteUser() {
-		
-		
+		RestAssured.baseURI = "https://dummy.restapiexample.com/api";
+		RestAssured.basePath = "/v1/delete/3";
+		String stringResponse = response.asPrettyString();
 	}
 
 }
