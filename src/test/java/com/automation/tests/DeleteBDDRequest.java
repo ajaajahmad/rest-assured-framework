d7231a18d0be797572ccf484772f12091c5add22
ajaajahmad <ajaaj75@gmail.com>
2025-10-12 23:34:23 +0530

    Rest Assured

--- src/test/java/com/automation/tests/DeleteBDDRequest.java
+++ src/test/java/com/automation/tests/DeleteBDDRequest.java
@@ -1,13 +1,25 @@
 package com.automation.tests;
 
 import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import io.restassured.response.ValidatableResponse;
 
 public class DeleteBDDRequest {
 	
-	
+	ValidatableResponse validateResponse;
 	
 	@Test
 	public void deleteUser() {
+		validateResponse = given()
+				.baseUri("https://dummy.restapiexample.com/api")
+				.basePath("/v1/delete/3")
+				.when()
+				.delete()
+				.then()
+				.assertThat().statusCode(200)
+				.body("message", equalTo("Successfully! Record has been deleted"));
+		System.out.println("Response: " + validateResponse.extract().asPrettyString());
 		
 	}
 
