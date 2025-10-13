99f88f70ec7589026d33f4ac817c2774e4637f9f
ajaajahmad <ajaaj75@gmail.com>
2025-10-13 23:01:56 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PostRequestJsonObject.java
+++ src/test/java/com/automation/tests/PostRequestJsonObject.java
@@ -1,7 +1,12 @@
 package com.automation.tests;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.json.simple.JSONObject;
 import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class PostRequestJsonObject {
 
@@ -14,7 +19,17 @@ public class PostRequestJsonObject {
 		data.put("profile_image", "test.png");
 		data.put("employee_age", "30");
 		data.put("employee_salary", "234");
-
+		
+		RestAssured.given()
+			.contentType(ContentType.JSON)
+			.body(data.toString())
+			.log().all()
+			.when()
+			.post("https://dummy.restapiexample.com/api/v1/create")
+			.then()
+			.assertThat().statusCode(200)
+			.body("message", equalTo("Successfully! Record has been added."))
+			.log().all();
 	}
 
 }
