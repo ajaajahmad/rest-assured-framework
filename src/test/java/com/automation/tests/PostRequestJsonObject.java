1dd1e3a818fd0843c7890a075b4740b5dc4532de
ajaajahmad <ajaaj75@gmail.com>
2025-10-13 23:12:35 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PostRequestJsonObject.java
+++ src/test/java/com/automation/tests/PostRequestJsonObject.java
@@ -10,25 +10,24 @@ import io.restassured.http.ContentType;
 
 public class PostRequestJsonObject {
 
-	@Test
-	public void passBodyAsJsonObject() {
+    @Test
+    public void passBodyAsJsonObject() {
+        
+    	JSONObject data = new JSONObject();
+        
+    	data.put("employee_name", "MapTest");
+        data.put("profile_image", "test.png");
+        data.put("employee_age", "30");
+        data.put("employee_salary", "234");
 
-		JSONObject data = new JSONObject();
-
-		data.put("employee_name", "MapTest");
-		data.put("profile_image", "test.png");
-		data.put("employee_age", "30");
-		data.put("employee_salary", "234");
-		
-		RestAssured.given()
-			.contentType(ContentType.JSON)
-			.body(data.toString())
-			.when()
-			.post("https://dummy.restapiexample.com/api/v1/create")
-			.then()
-			.assertThat().statusCode(200)
-			.body("message", equalTo("Successfully! Record has been added."))
-			.log().all();
-	}
-
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(data.toString())
+            .when()
+            .post("https://dummy.restapiexample.com/api/v1/create")
+            .then()
+            .assertThat().statusCode(200)
+            .body("message", equalTo("Successfully! Record has been added."))
+            .log().all();
+    }
 }
