6a4f7821b9079a78044f5b93a35d3a74871b4c14
ajaajahmad <ajaaj75@gmail.com>
2025-10-13 22:16:53 +0530

    Rest Assured

---
+++ src/test/java/com/automation/tests/PatchBDDRequest.java
@@ -0,0 +1,20 @@
+package com.automation.tests;
+
+import org.junit.Test;
+
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class PatchBDDRequest {
+	
+	RequestSpecification requestSpecification;
+	Response response;
+	ValidatableResponse validateResponse;
+	
+	@Test
+	public void updateUser() {
+		
+	}
+
+}
