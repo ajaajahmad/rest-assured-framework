41edc88383cf0065431a6bcbac1865315a42995b
ajaajahmad <ajaaj75@gmail.com>
2025-10-13 22:28:15 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PatchBDDRequest.java
+++ src/test/java/com/automation/tests/PatchBDDRequest.java
@@ -1,10 +1,14 @@
 package com.automation.tests;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static io.restassured.RestAssured.given;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
@@ -19,6 +23,17 @@ public class PatchBDDRequest {
 	public void updateUser() {
 		Map<String, String> map = new HashMap<>();
 		map.put("name", "William");
+		
+		validateResponse = given()
+				.baseUri("https://reqres.in/api/users/2")
+				.contentType(ContentType.JSON)
+				.header("x-api-key", "reqres-free-v1")
+				.body(map)
+				.when()
+				.patch()
+				.then()
+				.assertThat().statusCode(200)
+				.body("name", equalTo("William"));
 	}
 
 }
