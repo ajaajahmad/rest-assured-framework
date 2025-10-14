68952213111e96a18aa5b5f2f22d2a6c0c13c7c7
ajaajahmad <ajaaj75@gmail.com>
2025-10-14 23:46:41 +0530

    Rest Assured

--- src/test/java/com/automation/tests/JsonDemo.java
+++ src/test/java/com/automation/tests/JsonDemo.java
@@ -1,9 +1,17 @@
 package com.automation.tests;
 
+import org.json.simple.JSONObject;
+
 public class JsonDemo {
 	
 	public void passBodyAsJsonArray() {
 		//JSON Object for first employee
+		JSONObject firstEmployee = new JSONObject();
+		
+		firstEmployee.put("employee_name", "ajaaj");
+		firstEmployee.put("profile_image", "test.png");
+		firstEmployee.put("employee_age", "26");
+		firstEmployee.put("employee_salary", "11111");
 		
 		
 	}
