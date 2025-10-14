6111cebc339c028f102c43314beede18cb89f0c6
ajaajahmad <ajaaj75@gmail.com>
2025-10-14 23:56:58 +0530

    Rest Assured

--- src/test/java/com/automation/tests/JsonDemo.java
+++ src/test/java/com/automation/tests/JsonDemo.java
@@ -1,13 +1,15 @@
 package com.automation.tests;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.junit.Test;
 
 public class JsonDemo {
 
+	@Test
 	public void passBodyAsJsonArray() {
 		// JSON Object for first employee
 		JSONObject firstEmployee = new JSONObject();
-
 		firstEmployee.put("employee_name", "ajaaj");
 		firstEmployee.put("profile_image", "test.png");
 		firstEmployee.put("employee_age", "26");
@@ -15,14 +17,17 @@ public class JsonDemo {
 
 		// JSON Object for second employee
 		JSONObject secondEmployee = new JSONObject();
-
 		secondEmployee.put("employee_name", "ahmad");
 		secondEmployee.put("profile_image", "test.png");
 		secondEmployee.put("employee_age", "36");
-		secondEmployee.put("employee_salery", "99999");
+		secondEmployee.put("employee_salary", "99999");
 
-		// Create JSON Array to add both JSON objects
+		// Create JSON Array and add both JSON objects
+		JSONArray array = new JSONArray();
+		array.add(firstEmployee);
+		array.add(secondEmployee);
 
+		// Print JSON array (optional)
+		System.out.println(array.toJSONString());
 	}
-
 }
