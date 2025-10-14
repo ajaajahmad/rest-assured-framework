66e4cd4e6ad7c16a6ff6112c560f73cc1094ba38
ajaajahmad <ajaaj75@gmail.com>
2025-10-14 23:38:26 +0530

    Rest Assured

--- src/test/java/com/automation/tests/PostRequestJsonObject2.java
+++ src/test/java/com/automation/tests/PostRequestJsonObject2.java
@@ -13,36 +13,32 @@ public class PostRequestJsonObject2 {
     @Test
     public void passBodyAsJsonObject() {
         
-    	JSONObject data = new JSONObject();
-    	
-    	data.put("profile_image", "test.png");
+        JSONObject data = new JSONObject();
         
-    	JSONObject details = new JSONObject();
-    	
-    	details.put("updated_message", "Details of New Resource");
-        details.put("employee_age", "30");
+        data.put("profile_image", "test.png");
+        data.put("employee_name", "MapTest");
+        data.put("employee_salary", "11111");
         
-        details.put("employee_details", details);
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("employee_age", "30");
+        employeeDetails.put("updated_message", "Details of New Resource");
         
-        details.put("employee_name", "MapTest");
-        details.put("employee_salary", "11111");
+        data.put("employee_details", employeeDetails);
         
-
         RestAssured
-        
-        .given()
-            .contentType(ContentType.JSON)
-            .body(data.toString())
-        .when()
-            .post("https://dummy.restapiexample.com/api/v1/create")
-        .then()
-            .assertThat().statusCode(200)
-            .body("data.employee_name", equalTo("MapTest"))
-            .body("data.employee_details.employee_age", equalTo("30"))
-            .body("data.employee_details.updated_message", equalTo("Details of New Resource"))
-            .body("data.employee_salary", equalTo("11111"))
-            .body("data.profile_image", equalTo("test.png"))
-            .body("message", equalTo("Successfully! Record has been added."))
-            .log().all();
+            .given()
+                .contentType(ContentType.JSON)
+                .body(data.toJSONString())
+            .when()
+                .post("https://dummy.restapiexample.com/api/v1/create")
+            .then()
+                .assertThat().statusCode(200)
+                .body("data.employee_name", equalTo("MapTest"))
+                .body("data.employee_details.employee_age", equalTo("30"))
+                .body("data.employee_details.updated_message", equalTo("Details of New Resource"))
+                .body("data.employee_salary", equalTo("11111"))
+                .body("data.profile_image", equalTo("test.png"))
+                .body("message", equalTo("Successfully! Record has been added."))
+                .log().all();
     }
 }
