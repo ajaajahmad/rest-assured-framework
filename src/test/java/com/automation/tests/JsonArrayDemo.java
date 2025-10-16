29dfd99aa404340ab930cdfe366134adaeefcacf
ajaajahmad <ajaaj75@gmail.com>
2025-10-16 23:15:58 +0530

    Rest Assured

--- src/test/java/com/automation/tests/JsonArrayDemo.java
+++ src/test/java/com/automation/tests/JsonArrayDemo.java
@@ -11,6 +11,13 @@ public class JsonArrayDemo {
 		data1.put("lastname", "Ahmad");
 		data1.put("age", "25");
 		data1.put("salary", "116328");
+		
+		JSONObject data2 = new JSONObject();
+		
+		data2.put("firstname", "Shahrukh");
+		data2.put("lastname", "Khan");
+		data2.put("age", "54");
+		data2.put("salary", "742337");
 
 	}
 }
